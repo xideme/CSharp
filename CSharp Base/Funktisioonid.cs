6e23f811652d9ad871fd37f345812e4755a25b2c
xideme <soopka@gmail.com>
2024-04-04 09:56:33 +0300

    base ver

---
+++ CSharp Base/Funktisioonid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv23_C_põhikonstruktsioonid
+{
+    public class Funktisioonid
+    {
+        public static void Tervitus(string nimi)
+        {
+            Console.WriteLine("Tere kallis {0}", nimi);
+        }
+
+        public static int Korruta(int arv1, int arv2)            
+        {
+            return arv1*arv2;
+        }
+    }
+}
---
+++ CSharp Base/inimene.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv23_C_põhikonstruktsioonid
+{
+    internal class inimene
+    {
+        public string Nimi {get; set;}
+        public int Vanus { get; set;}
+        public inimene() {}
+        public inimene(string nimi)
+        {
+            Nimi = nimi;
+        }
+
+    }
+}
