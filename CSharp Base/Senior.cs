df18b7edefa773a8b4d01c74546a6be4b49cdb4d
xideme <soopka@gmail.com>
2024-04-18 09:11:26 +0300

    fixes

--- CSharp Base/Senior.cs
+++ CSharp Base/Senior.cs
@@ -8,9 +8,15 @@ namespace TARgv23_C_põhikonstruktsioonid
 {
     public class Senior : Inimene
     {
+        public int Tööstaž = 20;
+        public Senior() { }
+        public Senior(int tööstaz)
         {
+            Tööstaž = tööstaz;
         }
-
-
+        public override double Sissetulek(double tasu)
+        {
+            return (double)(tasu * (1 + (double)Tööstaž / 100));
+        }
     }
 }
