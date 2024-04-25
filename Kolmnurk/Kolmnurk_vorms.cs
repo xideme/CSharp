f13ce1fb25101623037aa1be09c45376957bcb47
xideme <soopka@gmail.com>
2024-04-25 09:42:07 +0300

    first

---
+++ Kolmnurk/Kolmnurk_vorms.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kolmnurk
+{
+    public partial class Kolmnurk_vorms : Form
+    {
+        public Kolmnurk_vorms()
+        {
+            InitializeComponent();
+        }
+
+        private void Kolmnurk_vorms_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kolmnurk/Triangle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Kolmnurk
+{
+    public class Triangle
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public bool ExistTriangle
+        {
+            get
+            {
+                if (A + B > C && A + C > B && B + C > A)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public double GetSetA
+        {
+            get { return A; }
+            set { A = value; }
+        }
+
+        public double GetSetC
+        {
+            get { return C; }
+            set { C = value; }
+        }
+
+        public double Perimeter()
+        {
+            return A + B + C;
+        }
+
+        public double Area()
+        {
+            double s = 0;
+            if (ExistTriangle)
+            {
+                double p = (A + B + C) / 2;
+                s = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            }
+            return s;
+        }
+
+        public string OutputA()
+        {
+            return A.ToString();
+        }
+
+        public string OutputB()
+        {
+            return B.ToString();
+        }
+
+        public string OutputC()
+        {
+            return C.ToString();
+        }
+    }
+}
+
+
+
