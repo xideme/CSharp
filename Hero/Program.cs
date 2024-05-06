bed6409510ef0fdce9794eb690af5e72a6aa83ef
Martin Rosman <soopka@gmail.com>
2024-05-06 18:16:11 +0300

    first ed

---
+++ Hero/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Hero
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+
+    public Hero(string name, string location)
+    {
+        Name = name;
+        Location = location;
+    }
+
+    public virtual int Save(int endangeredPeople)
+    {
+        return (int)Math.Round(endangeredPeople * 0.95);
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Location: {Location}";
+    }
+}
+
+class SuperHero : Hero
+{
+    public double SkillLevel { get; }
+
+    public SuperHero(string name, string location, double skillLevel) : base(name, location)
+    {
+        SkillLevel = skillLevel;
+    }
+
+    public override int Save(int dangerPeople)
+    {
+        return (int)Math.Round(dangerPeople * (0.95) +SkillLevel);
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $", Skill Level: {SkillLevel}";
+    }
+}
+
+class Program
+{
+    static List<Hero> heroesList = new List<Hero>();
+
+    static void Main(string[] args)
+    {
+        ReadHeroesFromFile("../../../heroes.txt");
+
+        foreach (var hero in heroesList)
+        {
+            Console.WriteLine(hero.ToString());
+            if (hero is SuperHero)
+            {
+                SuperHero superHero = (SuperHero)hero;
+                int inDanger = 450;
+                Console.WriteLine($"People in Danger: {inDanger}");
+                int savedPeople = superHero.Save(inDanger); // 450 people in danger
+                Console.WriteLine($"Saved people: {savedPeople}");
+            }
+            else
+            {
+                int inDanger = 450;
+                Console.WriteLine($"People in Danger: {inDanger}");
+                int savedPeople = hero.Save(inDanger); // 450 people in danger
+                Console.WriteLine($"Saved people: {savedPeople}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void ReadHeroesFromFile(string fileName)
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] data = line.Split('/');
+                    string name = data[0].Trim();
+                    string location = data[1].Trim();
+
+                    if (name.EndsWith("*"))
+                    {
+                        int skillLevel = new Random().Next(1, 6); // random skill level between 1 and 5
+                        heroesList.Add(new SuperHero(name.TrimEnd('*'), location, skillLevel));
+                    }
+                    else
+                    {
+                        heroesList.Add(new Hero(name, location));
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
